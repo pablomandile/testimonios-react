9e7917a40151485acf548eb176b8ef7811088179
pablo.mandile@bue.edu.ar <pablo.mandile@bue.edu.ar>
2022-11-30 21:05:35 -0300

    estructura y primer componente para mostrar tarjeta de testimonio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/testimonio'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='contenedor-principal'>
+          <h1>Esto es lo que dicen nuestros alumnos de nosotros</h1>
+          <Testimonio 
+            nombre='Aida Borsch'
+            pais='Suecia'
+            imagen='Aida'
+            cargo='Ingeniera de software'
+            empresa='Spotify'
+            testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante nunc, semper a varius dapibus, imperdiet at enim. Fusce quis tortor mi. Nunc egestas, nibh eget auctor eleifend, velit dui facilisis neque, non iaculis sem justo a enim. Donec sed turpis laoreet, eleifend ligula vel, ultricies nibh. Cras sed lacus volutpat, egestas massa eget, consectetur erat. Suspendisse urna dolor, tincidunt eget augue ut, pretium dictum sem. Nunc finibus est nec hendrerit sagittis. Mauris venenatis lacinia magna, ac scelerisque felis egestas quis. Morbi in fermentum lectus.' />
+          <Testimonio 
+            nombre='Jimmy Egestas'
+            pais='North América'
+            imagen='Jimmi'
+            cargo='Proyect manager'
+            empresa='Avast'
+            testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante nunc, semper a varius dapibus, imperdiet at enim. Fusce quis tortor mi. Nunc egestas, nibh eget auctor eleifend, velit dui facilisis neque, non iaculis sem justo a enim. Donec sed turpis laoreet, eleifend ligula vel, ultricies nibh. Cras sed lacus volutpat, egestas massa eget, consectetur erat. Suspendisse urna dolor, tincidunt eget augue ut, pretium dictum sem. Nunc finibus est nec hendrerit sagittis. Mauris venenatis lacinia magna, ac scelerisque felis egestas quis. Morbi in fermentum lectus.' />
+      </div>
     </div>
   );
 }
---
+++ src/componentes/testimonio.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import '../css/testimonio.css';
+
+function Testimonio(props){
+  return(
+    <div className='contenedor-testimonio'>
+      <img 
+        className='img-testimonio'
+        src={require(`../img/testimonio-${props.imagen}.jpg`)} 
+        alt='foto de Aida'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div> 
+    </div>
+  );
+}
+
+export default Testimonio; 
